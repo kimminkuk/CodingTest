56d0a766af34d1e90f0856666e6c500d79ba8c81
mikuk <rlaal567@gmail.com>
2021-12-30 21:18:17 +0900

    Update beak_5052.py

--- beak_5052.py
+++ beak_5052.py
@@ -26,3 +26,33 @@ for i in range(0, testNum):
     test.clear()
 for i in result:
     print(i)
+
+    
+#why...
+import sys
+input = sys.stdin.readline
+def sol(arr):
+    arrSort = sorted(arr, reverse=False)
+    arrSort = list(map(str,arrSort))
+    arrSort_len = len(arrSort)
+    #print(arrSort)
+
+    for i in range(0, arrSort_len-1):
+        aSFL=len(arrSort[i])
+        #print('i->',i,'arr_left: ',arrSort[i], 'arr_right',arrSort[i+1][:aSFL])
+        if arrSort[i] == arrSort[i+1][:aSFL]:
+            answer = 'NO'
+            return answer
+
+    answer = 'YES'
+    return answer
+
+testNum = int(input())
+for i in range(0, testNum):
+    testNum2 = int(input())
+    test = []
+    for j in range(0, testNum2):
+        #test.append(int(input()))
+        #test.append(input())
+        test.append(input().strip())
+    print(sol(test))
